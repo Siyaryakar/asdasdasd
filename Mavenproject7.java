b5f617824e2aeabd7f4b7ea65ec5269eec55d4b8
Siyaryakar <44892602+Siyaryakar@users.noreply.github.com>
2025-10-18 01:01:00 +0300

    Add files via upload

---
+++ Mavenproject7.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mavenproject7;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author siyaryakar
+ */
+public class Mavenproject7 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        
+        int secim;
+        int bakiye;
+        System.out.println("Seçim yapın 1-su(3tl) ....");
+        secim=sc.nextInt();
+        System.out.println("bakiyenizi girin");
+        bakiye=sc.nextInt();
+        
+        switch(secim){
+        
+            case 1:
+            case 2:
+                
+            default:
+                
+        
+        
+        
+        
+        }
+
+    }
+}
